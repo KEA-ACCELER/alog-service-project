e7bfb6c07e48896145b0157d2365446fc0cc394b
Yejin Choi <63734765+chldppwls12@users.noreply.github.com>
2023-07-04 14:24:49 +0900

    refactor: entity
    - 양방향 관계 설정
    - 네이밍 통일

---
+++ src/main/java/kea/alog/project/domain/project/entity/Project.java
@@ -0,0 +1,71 @@
+package kea.alog.project.domain.project.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import kea.alog.project.common.entity.BaseEntity;
+import kea.alog.project.domain.projectMember.entity.ProjectMember;
+import kea.alog.project.domain.topic.entity.Topic;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Table(name = "project")
+@NoArgsConstructor(access = lombok.AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Getter
+@ToString
+public class Project extends BaseEntity implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "pk")
+    private Long pk;
+
+    @Column(name = "name", length = 60)
+    private String name;
+
+    @Column(name = "description", length = 200)
+    private String description;
+
+    @Column(name = "team_name", length = 10)
+    private String teamName;
+
+    @Column(name = "team_pk")
+    private Long teamPk;
+
+    @Column(name = "pm_nn", length = 10)
+    private String pmNn;
+
+    @Column(name = "pm_pk")
+    private Long pmPk;
+
+    @OneToMany(mappedBy = "project")
+    private List<Topic> topics = new ArrayList<>();
+
+    @OneToMany(mappedBy = "userPk")
+    private List<ProjectMember> projectMembers = new ArrayList<>();
+
+    @Builder
+    public Project(String name, String description, String teamName,
+        Long teamPk, String pmNn, Long pmPk) {
+        this.name = name;
+        this.description = description;
+        this.teamName = teamName;
+        this.teamPk = teamPk;
+        this.pmNn = pmNn;
+        this.pmPk = pmPk;
+    }
+
+
+}
