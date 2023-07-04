9e779a0c78464c0731ec568315c1495e5e311f3f
Yejin Choi <63734765+chldppwls12@users.noreply.github.com>
2023-07-04 14:39:45 +0900

    feat: responseDto

---
+++ src/main/java/kea/alog/project/common/dto/ResponseDto.java
@@ -0,0 +1,38 @@
+package kea.alog.project.common.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class ResponseDto<T> {
+
+    private final int code;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private final String message;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private T data;
+
+    public static <T> ResponseDto<T> success(int code, T data) {
+        return ResponseDto.<T>builder()
+                          .code(code)
+                          .data(data)
+                          .build();
+    }
+
+    public static <T> ResponseDto<T> fail(int code, String message) {
+        return ResponseDto.<T>builder()
+                          .code(code)
+                          .message(message)
+                          .build();
+    }
+
+    @Builder
+    public ResponseDto(int code, String message, T data) {
+        this.code = code;
+        this.message = message;
+        this.data = data;
+    }
+}
