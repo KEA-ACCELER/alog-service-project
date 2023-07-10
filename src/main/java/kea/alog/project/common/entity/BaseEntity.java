cf1f62a9088948dad5360a99efd60d0cbb422154
Yejin Choi <63734765+chldppwls12@users.noreply.github.com>
2023-07-10 14:47:46 +0900

    refactor: add status in baseEntity

--- src/main/java/kea/alog/project/common/entity/BaseEntity.java
+++ src/main/java/kea/alog/project/common/entity/BaseEntity.java
@@ -2,11 +2,15 @@ package kea.alog.project.common.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.MappedSuperclass;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
+import jakarta.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import kea.alog.project.common.constant.Status;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
@@ -16,12 +20,19 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public class BaseEntity {
+
     @CreatedDate
     @Column(updatable = false)
+    @NotNull
     private LocalDateTime createdAt;
 
     @LastModifiedDate
+    @NotNull
     private LocalDateTime updatedAt;
+
+    @Enumerated(EnumType.ORDINAL)
+    @NotNull
+    private Status status = Status.NORMAL;
 
     // Asia/Seoul로 변환할 때 사용
     @PrePersist
