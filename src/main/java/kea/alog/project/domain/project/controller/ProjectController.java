6aa437f002113849495f526aea470f8730269e51
Yejin Choi <63734765+chldppwls12@users.noreply.github.com>
2023-07-14 11:35:18 +0900

    refactor: ResponseDto generic type

--- src/main/java/kea/alog/project/domain/project/controller/ProjectController.java
+++ src/main/java/kea/alog/project/domain/project/controller/ProjectController.java
@@ -2,9 +2,12 @@ package kea.alog.project.domain.project.controller;
 
 
 import jakarta.validation.Valid;
+import kea.alog.project.common.dto.PageDto;
 import kea.alog.project.common.dto.ResponseDto;
 import kea.alog.project.domain.project.constant.ProjectSortType;
 import kea.alog.project.domain.project.dto.request.CreateProjectRequestDto;
+import kea.alog.project.domain.project.dto.response.CreateProjectResponseDto;
+import kea.alog.project.domain.project.dto.response.ProjectDto;
 import kea.alog.project.domain.project.service.ProjectService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,19 +26,21 @@ public class ProjectController {
     private final ProjectService projectService;
 
     @PostMapping()
-    public ResponseDto create(@Valid @RequestBody CreateProjectRequestDto createProjectRequestDto) {
+    public ResponseDto<CreateProjectResponseDto> create(
+        @Valid @RequestBody CreateProjectRequestDto createProjectRequestDto) {
         return ResponseDto.success(201, projectService.create(createProjectRequestDto));
     }
 
     @GetMapping()
-    public ResponseDto findAll(@RequestParam(value = "keyword", required = false) String keyword,
+    public ResponseDto<PageDto<ProjectDto>> findAll(
+        @RequestParam(value = "keyword", required = false) String keyword,
         @RequestParam("sortType") ProjectSortType sortType, @RequestParam("page") int page,
         @RequestParam("size") int size) {
         return ResponseDto.success(200, projectService.findAll(keyword, sortType, page, size));
     }
 
     @GetMapping("/{projectPk}")
-    public ResponseDto findOne(@PathVariable("projectPk") Long projectPk) {
+    public ResponseDto<ProjectDto> findOne(@PathVariable("projectPk") Long projectPk) {
         return ResponseDto.success(200, projectService.findOne(projectPk));
     }
 }
