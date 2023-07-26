4440d9084085f83e4426625acd2e60ef51c8965c
Yejin Choi <63734765+chldppwls12@users.noreply.github.com>
2023-07-26 13:39:29 +0900

    Feat/swagger (#31)
    * feat: swagger config
    
    * feat: cors config

--- src/main/java/kea/alog/project/domain/project/controller/ProjectController.java
+++ src/main/java/kea/alog/project/domain/project/controller/ProjectController.java
@@ -1,6 +1,8 @@
 package kea.alog.project.domain.project.controller;
 
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import kea.alog.project.common.dto.PageDto;
 import kea.alog.project.common.dto.ResponseDto;
@@ -21,6 +23,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "projects", description = "프로젝트 API")
 @RequestMapping("/api/projects")
 @RestController
 @RequiredArgsConstructor
@@ -28,12 +31,14 @@ public class ProjectController {
 
     private final ProjectService projectService;
 
+    @Operation(summary = "프로젝트 생성")
     @PostMapping()
     public ResponseDto<ProjectPkResponseDto> create(
         @Valid @RequestBody CreateProjectRequestDto createProjectRequestDto) {
         return ResponseDto.success(201, projectService.create(createProjectRequestDto));
     }
 
+    @Operation(summary = "프로젝트 전체 조회")
     @GetMapping()
     public ResponseDto<PageDto<ProjectDto>> findAll(
         @RequestParam(value = "keyword", required = false) String keyword,
@@ -42,11 +47,13 @@ public class ProjectController {
         return ResponseDto.success(200, projectService.findAll(keyword, sortType, page, size));
     }
 
+    @Operation(summary = "프로젝트 상세 조회")
     @GetMapping("/{projectPk}")
     public ResponseDto<ProjectDto> findOne(@PathVariable("projectPk") Long projectPk) {
         return ResponseDto.success(200, projectService.findOne(projectPk));
     }
 
+    @Operation(summary = "프로젝트 수정")
     @PatchMapping("/{projectPk}")
     public ResponseDto<ProjectPkResponseDto> update(
         @PathVariable("projectPk") Long projectPk,
@@ -55,6 +62,7 @@ public class ProjectController {
         return ResponseDto.success(200, projectService.update(projectPk, updateProjectRequestDto));
     }
 
+    @Operation(summary = "프로젝트 삭제")
     @DeleteMapping("/{projectPk}")
     public ResponseDto<ProjectPkResponseDto> delete(@PathVariable("projectPk") Long projectPk) {
         return ResponseDto.success(200, projectService.delete(projectPk));
--- src/main/java/kea/alog/project/domain/topic/controller/TopicController.java
+++ src/main/java/kea/alog/project/domain/topic/controller/TopicController.java
@@ -1,5 +1,7 @@
 package kea.alog.project.domain.topic.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import kea.alog.project.common.dto.PageDto;
 import kea.alog.project.common.dto.ResponseDto;
@@ -20,6 +22,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "tags", description = "태그 API")
 @RequestMapping("/api/projects/{projectPk}/topics")
 @RestController
 @RequiredArgsConstructor
@@ -27,6 +30,7 @@ public class TopicController {
 
     private final TopicService topicService;
 
+    @Operation(summary = "토픽 전체 조회")
     @GetMapping()
     public ResponseDto<PageDto<TopicDto>> findAll(
         // TODO: dto로 param 한 번에 받는 방법
@@ -40,6 +44,7 @@ public class TopicController {
             topicService.findAll(projectPk, keyword, sortType, page, size));
     }
 
+    @Operation(summary = "토픽 상세 조회")
     @GetMapping("/{topicPk}")
     public ResponseDto<TopicDto> findOne(
         @PathVariable("projectPk") Long projectPk,
@@ -47,12 +52,14 @@ public class TopicController {
         return ResponseDto.success(200, topicService.findOne(projectPk, topicPk));
     }
 
+    @Operation(summary = "토픽 생성")
     @PostMapping
     public ResponseDto<TopicPkResponseDto> create(@PathVariable("projectPk") Long projectPk,
         @Valid @RequestBody CreateTopicRequestDto createTopicRequestDto) {
         return ResponseDto.success(201, topicService.create(projectPk, createTopicRequestDto));
     }
 
+    @Operation(summary = "토픽 수정")
     @PatchMapping("/{topicPk}")
     public ResponseDto<TopicPkResponseDto> update(@PathVariable("projectPk") Long projectPk,
         @PathVariable("topicPk") Long topicPk,
@@ -61,6 +68,7 @@ public class TopicController {
             topicService.update(projectPk, topicPk, updateTopicRequestDto));
     }
 
+    @Operation(summary = "토픽 삭제")
     @DeleteMapping("/{topicPk}")
     public ResponseDto delete(@PathVariable("projectPk") Long projectPk,
         @PathVariable("topicPk") Long topicPk) {
