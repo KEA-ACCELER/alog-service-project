4de4df5b1d4b6db3f172487d6bb888b118f7c926
Yejin Choi <63734765+chldppwls12@users.noreply.github.com>
2023-08-07 12:33:57 +0900

    refactor: find all members return type
    - ProjectMemberResponseDto -> Long

--- src/main/java/kea/alog/project/domain/projectMember/service/ProjectMemberService.java
+++ src/main/java/kea/alog/project/domain/projectMember/service/ProjectMemberService.java
@@ -2,11 +2,10 @@ package kea.alog.project.domain.projectMember.service;
 
 import kea.alog.project.common.dto.PageDto;
 import kea.alog.project.domain.projectMember.dto.request.ProjectMemberRequestDto;
-import kea.alog.project.domain.projectMember.dto.response.ProjectMemberResponseDto;
 
 public interface ProjectMemberService {
 
-    PageDto<ProjectMemberResponseDto> findAll(Long projectPk, String keyword, int page, int size);
+    PageDto<Long> findAll(Long projectPk, String keyword, int page, int size);
 
     void join(Long projectPk, ProjectMemberRequestDto ProjectMemberRequestDto);
 
