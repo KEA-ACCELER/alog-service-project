58fcf83d6437f32e326e34f37f5ea458decef39a
Yejin Choi <63734765+chldppwls12@users.noreply.github.com>
2023-08-08 23:47:01 +0900

    refactor: add bearer scheme

--- src/main/java/kea/alog/project/config/SecurityConfig.java
+++ src/main/java/kea/alog/project/config/SecurityConfig.java
@@ -20,7 +20,7 @@ public class SecurityConfig {
         "v3/api-docs/**",
         "/swagger-ui/**",
         "swagger-ui/**",
-        "/api/projects/swagger-ui/**"
+        "/api/projects/**"
     };
 
 
