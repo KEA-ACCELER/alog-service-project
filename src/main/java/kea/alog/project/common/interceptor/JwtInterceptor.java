12386b39dd0000a13b29753e5bc2debee8c66322
Yejin Choi <63734765+chldppwls12@users.noreply.github.com>
2023-08-09 09:47:03 +0900

    refactor: get user info in payload

--- src/main/java/kea/alog/project/common/interceptor/JwtInterceptor.java
+++ src/main/java/kea/alog/project/common/interceptor/JwtInterceptor.java
@@ -1,7 +1,9 @@
 package kea.alog.project.common.interceptor;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import kea.alog.project.common.dto.TokenPayloadDto;
 import kea.alog.project.common.exception.UnauthorizationException;
 import kea.alog.project.common.util.JwtProvider;
 import lombok.RequiredArgsConstructor;
@@ -15,12 +17,14 @@ public class JwtInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response,
-        Object handler) {
+        Object handler) throws JsonProcessingException {
         String token = request.getHeader("Authorization");
         if (token == null) {
             throw new UnauthorizationException(403, "AUTHORIZATION");
         }
-        request.setAttribute("user", jwtProvider.getPayload(token.replace("Bearer ", "")));
+        String payload = jwtProvider.getPayload(token.replace("Bearer ", ""));
+        TokenPayloadDto user = jwtProvider.getUserInfo(payload);
+        request.setAttribute("user", user);
         return true;
     }
 }
--- src/main/java/kea/alog/project/common/util/JwtProvider.java
+++ src/main/java/kea/alog/project/common/util/JwtProvider.java
@@ -2,6 +2,10 @@ package kea.alog.project.common.util;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.interfaces.DecodedJWT;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.Base64;
+import kea.alog.project.common.dto.TokenPayloadDto;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -9,7 +13,13 @@ public class JwtProvider {
 
     public String getPayload(String token) {
         DecodedJWT jwt = JWT.decode(token);
-        System.out.println(jwt);
         return jwt.getPayload();
+    }
+
+    public TokenPayloadDto getUserInfo(String payload) throws JsonProcessingException {
+        Base64.Decoder decoder = Base64.getUrlDecoder();
+        String str = new String(decoder.decode(payload));
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.readValue(str, TokenPayloadDto.class);
     }
 }
