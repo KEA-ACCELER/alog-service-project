9c1343327b8b198fe6fa733b53bddbe1d72276ee
Yejin Choi <63734765+chldppwls12@users.noreply.github.com>
2023-08-10 10:37:49 +0900

    Refactor/swagger (#59)
    * refactor: topic api response
    
    * refactor: project member api response
    
    * refactor: project api response
    
    * refactor: ProjectDto

--- src/main/java/kea/alog/project/domain/topic/controller/TopicController.java
+++ src/main/java/kea/alog/project/domain/topic/controller/TopicController.java
@@ -1,6 +1,10 @@
 package kea.alog.project.domain.topic.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
@@ -33,6 +37,11 @@ public class TopicController {
     private final TopicService topicService;
 
     @Operation(summary = "토픽 전체 조회")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "성공"),
+        @ApiResponse(responseCode = "400", description = "유효하지 않은 request", content = @Content(schema = @Schema(implementation = ResponseDto.class))),
+        @ApiResponse(responseCode = "403", description = "권한 없음", content = @Content(schema = @Schema(implementation = ResponseDto.class)))
+    })
     @GetMapping()
     public ResponseDto<PageDto<TopicDto>> findAll(
         // TODO: dto로 param 한 번에 받는 방법
@@ -47,6 +56,11 @@ public class TopicController {
     }
 
     @Operation(summary = "토픽 상세 조회")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "성공"),
+        @ApiResponse(responseCode = "403", description = "권한 없음", content = @Content(schema = @Schema(implementation = ResponseDto.class))),
+        @ApiResponse(responseCode = "404", description = "존재하지 않는 토픽 pk", content = @Content(schema = @Schema(implementation = ResponseDto.class)))
+    })
     @GetMapping("/{topicPk}")
     public ResponseDto<TopicDto> findOne(
         @PathVariable("projectPk") Long projectPk,
@@ -55,6 +69,11 @@ public class TopicController {
     }
 
     @Operation(summary = "토픽 생성")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "201", description = "성공"),
+        @ApiResponse(responseCode = "400", description = "유효하지 않은 request", content = @Content(schema = @Schema(implementation = ResponseDto.class))),
+        @ApiResponse(responseCode = "403", description = "권한 없음", content = @Content(schema = @Schema(implementation = ResponseDto.class))),
+    })
     @PostMapping
     public ResponseDto<TopicPkResponseDto> create(
         HttpServletRequest request, @PathVariable("projectPk") Long projectPk,
@@ -65,6 +84,11 @@ public class TopicController {
     }
 
     @Operation(summary = "토픽 수정")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "201", description = "성공"),
+        @ApiResponse(responseCode = "400", description = "유효하지 않은 request", content = @Content(schema = @Schema(implementation = ResponseDto.class))),
+        @ApiResponse(responseCode = "403", description = "권한 없음", content = @Content(schema = @Schema(implementation = ResponseDto.class))),
+    })
     @PatchMapping("/{topicPk}")
     public ResponseDto update(@PathVariable("projectPk") Long projectPk,
         @PathVariable("topicPk") Long topicPk,
@@ -74,11 +98,16 @@ public class TopicController {
     }
 
     @Operation(summary = "토픽 삭제")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "204", description = "성공"),
+        @ApiResponse(responseCode = "403", description = "권한 없음", content = @Content(schema = @Schema(implementation = ResponseDto.class))),
+        @ApiResponse(responseCode = "404", description = "존재하지 않는 토픽 pk", content = @Content(schema = @Schema(implementation = ResponseDto.class)))
+    })
     @DeleteMapping("/{topicPk}")
     public ResponseDto delete(@PathVariable("projectPk") Long projectPk,
         @PathVariable("topicPk") Long topicPk) {
         topicService.delete(projectPk, topicPk);
-        return ResponseDto.success(200);
+        return ResponseDto.success(204);
     }
 
 }
